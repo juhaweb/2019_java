fb8780c05423b3cc796709c534365796490359ea
juhaweb <u66kie@naver.com>
2019-10-18 10:15:29 +0900

    2019/10/18 프로젝트

---
+++ ch04/src/p124/ForMultiplicationTableExample.java
@@ -0,0 +1,33 @@
+package p124;
+
+public class ForMultiplicationTableExample {
+
+	public static void main(String[] args) {
+				
+		for (int m=2; m<=9; m++) {
+		
+			System.out.println("*** " + m + "´Ü ***");
+			
+			for (int n=1; n<=9; n++) {
+				
+				System.out.println(m +"*"+ n +"="+ (m*n));
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ ch04/src/p125/WhileSumFrome1To100Example.java
@@ -0,0 +1,31 @@
+package p125;
+
+public class WhileSumFrome1To100Example {
+
+	
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int i = 1;
+		
+		while(i<=100) {
+			sum += i;
+			i++;
+			
+		}
+		
+		
+		
+		// 1부터 100까지의 합  
+		System.out.println(sum);
+		
+		
+		
+		
+		
+	} 
+	
+	
+	
+	
+}
