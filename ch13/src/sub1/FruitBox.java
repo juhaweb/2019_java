88aafe327753526c5f02bc320cf8fcc6c139c20c
juhaweb <u66kie@naver.com>
2019-10-25 12:38:27 +0900

    20191025

--- ch13/src/sub1/FruitBox.java
+++ ch13/src/sub1/FruitBox.java
@@ -6,12 +6,12 @@ public class FruitBox<T> {
 	private T fruit;
 	
 	// Getter : 외부로 클래스의 멤버를 전달하기 위한 메서드 
-	public T getF() {
+	public T getFruit() {
 		return fruit;
 	}
 	
 	// Setter : 외부에서 클래스의 멤버를 전달 받기 위한 메서드
-	public void setF(T fruit) {
+	public void setFruit(T fruit) {
 		this.fruit = fruit;
 	}
 		
--- ch13/src/sub1/GenericTest.java
+++ ch13/src/sub1/GenericTest.java
@@ -1,10 +1,5 @@
 package sub1;
-/*
- * 날짜 : 2019.10.24
- * 이름 : 하정욱
- * 내용 : 제네릭클래스 실습하기 교재 p654
- * 
- */
+
 public class GenericTest {
 
 	public static void main(String[] args) {
@@ -14,15 +9,15 @@ public class GenericTest {
 
 		FruitBox<Apple> box1 = new FruitBox<>();
 		box1.setFruit(a);
-											// <>만 쓰기도 함. 
+											
 		FruitBox<Banana> box2 = new FruitBox<>();
 		box2.setFruit(b);
 		
 		Apple	fr1 = box1.getFruit();
 		Banana	fr2 = box2.getFruit();
 		
-		f1.info();
-		f2.info();
+		fr1.info();
+		fr2.info();
 		
 		
 		
